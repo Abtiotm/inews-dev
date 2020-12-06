84c82d437d0285d72c1a9a1b8de49d3054c1cac2
obito-segu <925394401@qq.com>
2020-12-06 19:00:20 +0800

    feat: service-files

---
+++ inews-dev-service-files/src/main/java/the/eric/files/controller/HelloController.java
@@ -0,0 +1,19 @@
+package the.eric.files.controller;
+
+import the.eric.api.controller.user.HelloControllerApi;
+import the.eric.grace.result.GraceJSONResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController implements HelloControllerApi {
+
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    public Object hello() {
+        return GraceJSONResult.ok("Hello World!");
+    }
+
+
+}
